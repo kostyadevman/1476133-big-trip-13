f29bedba1adfc250681c80155975f5240015e8b5
Konstantin <you@example.com>
2020-11-24 21:01:31 +0300

    Create structure for tripPoint;Add mock;Change point's render template

--- src/main.js
+++ src/main.js
@@ -3,16 +3,23 @@ import {tripCostTemplate} from "./view/trip-cost";
 import {siteMenuTemplate} from "./view/site-menu";
 import {filterTemplate} from "./view/filter";
 import {sortTemplate} from "./view/sort";
-import {formCreateTemplate} from "./view/form-create";
-import {formEditTemplate} from "./view/form-edit";
+// import {formCreateTemplate} from "./view/form-create";
+// import {formEditTemplate} from "./view/form-edit";
 import {tripPointListTemplate} from "./view/trip-point-list";
 import {tripPointTemplate} from "./view/trip-point";
+import {generateTripPoint} from "./mock/trip-point";
+const TRIP_POINT_COUNT = 20;
+
+const points = new Array(TRIP_POINT_COUNT).fill().map(generateTripPoint);
+
+const pointsSorted = points.sort((a, b) => {
+  return a.date - b.date;
+});
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const TRIP_POINT_COUNT = 3;
 const siteHeaderElement = document.querySelector(`.trip-main`);
 const siteMainElement = document.querySelector(`.page-main`);
 
@@ -23,11 +30,11 @@ render(siteHeaderElement.querySelector(`.trip-controls h2:last-child`), filterTe
 
 render(siteMainElement.querySelector(`.trip-events`), sortTemplate(), `beforeend`);
 render(siteMainElement.querySelector(`.trip-events`), tripPointListTemplate(), `beforeend`);
-for (let i = 0; i < TRIP_POINT_COUNT; i++) {
-  render(siteMainElement.querySelector(`.trip-events__list`), tripPointTemplate(), `beforeend`);
+for (let i = 1; i < TRIP_POINT_COUNT; i++) {
+  render(siteMainElement.querySelector(`.trip-events__list`), tripPointTemplate(pointsSorted[i]), `beforeend`);
 }
-const points = siteMainElement.querySelectorAll(`.trip-events__item`);
-points[0].innerHTML = ``;
-render(points[0], formEditTemplate(), `afterbegin`);
-points[points.length - 1].innerHTML = ``;
-render(points[points.length - 1], formCreateTemplate(), `afterbegin`);
+// const points = siteMainElement.querySelectorAll(`.trip-events__item`);
+// points[0].innerHTML = ``;
+// render(pointsSorted[0], formEditTemplate(), `afterbegin`);
+// points[pointsSorted.length - 1].innerHTML = ``;
+// render(pointsSorted[pointsSorted.length - 1], formCreateTemplate(), `afterbegin`);
---
+++ src/mock/trip-point.js
@@ -0,0 +1,144 @@
+import {getRandomInteger} from "../utils";
+import dayjs from "dayjs";
+import {nanoid} from 'nanoid';
+
+const PHOTO_COUNT_MAX = 4;
+const OFFER_COUNT = 20;
+const POINT_OFFER_COUNT_MAX = 5;
+const PRICE_MAX = 120;
+
+const generateType = () => {
+  const tripPointTypes = [
+    `Taxi`,
+    `Bus`,
+    `Train`,
+    `Ship`,
+    `Transport`,
+    `Drive`,
+    `Flight`,
+    `Check-in`,
+    `Sightseeing`,
+    `Restaurant`
+  ];
+
+  const randomIndex = getRandomInteger(0, tripPointTypes.length - 1);
+
+  return tripPointTypes[randomIndex];
+};
+
+const generateDestination = () => {
+  const destinations = [
+    `Amsterdam`,
+    `Chamonix`,
+    `Geneva`,
+    `London`,
+    `Paris`,
+  ];
+
+  const randomIndex = getRandomInteger(0, destinations.length - 1);
+
+  return destinations[randomIndex];
+};
+
+const generateDescription = () => {
+  const descriptions = [
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    `Cras aliquet varius magna, non porta ligula feugiat eget.`,
+    `Fusce tristique felis at fermentum pharetra.`,
+    `Aliquam id orci ut lectus varius viverra.`,
+    `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
+    `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+    `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
+    `Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat.`,
+    `Nunc fermentum tortor ac porta dapibus.`,
+    `In rutrum ac purus sit amet tempus.`
+  ];
+
+  const randomIndex = getRandomInteger(0, descriptions.length - 1);
+
+  return descriptions[randomIndex];
+};
+
+const generatePrice = () => {
+  return getRandomInteger(0, PRICE_MAX);
+};
+
+const generateOffers = () => {
+  const offerTitles = [
+    `Order Uber`,
+    `Add luggage`,
+    `Switch to comfort`,
+    `Rent a car`,
+    `Add breakfast`,
+    `Book tickets`,
+    `Lunch in city`
+  ];
+  const generateTitle = () => {
+    const randomIndex = getRandomInteger(0, offerTitles.length - 1);
+
+    return offerTitles[randomIndex];
+  };
+
+  const generateOffer = () => {
+    return {
+      id: nanoid(),
+      type: generateType(),
+      title: generateTitle(),
+      price: generatePrice()
+    };
+  };
+
+  return new Array(OFFER_COUNT).fill().map(generateOffer);
+};
+
+const offers = generateOffers();
+
+const getOffers = (type) => {
+  const offerCount = getRandomInteger(0, POINT_OFFER_COUNT_MAX);
+  return offers.filter((item) => item.type === type).slice(0, offerCount) || [];
+};
+
+const generatePhotos = () => {
+  const photos = [];
+  const photo = `http://picsum.photos/248/152?r=${Math.random()}`;
+  photos.push(photo);
+
+  for (let i = 1; i < getRandomInteger(0, PHOTO_COUNT_MAX); i++) {
+    photos.push(photo);
+  }
+  return photo;
+};
+
+const generateDate = () => {
+  const maxDaysGap = 7;
+  const daysGap = getRandomInteger(-maxDaysGap, maxDaysGap);
+
+  return dayjs().add(daysGap, `day`).toDate();
+};
+
+const addDuration = (date) => {
+  const maxHourDuration = 24;
+  const maxMinuteDuration = 60;
+
+  return dayjs(date).add(getRandomInteger(0, maxHourDuration), `hour`)
+    .add(getRandomInteger(0, maxMinuteDuration), `minute`).toDate();
+};
+
+export const generateTripPoint = () => {
+  const type = generateType();
+  const date = generateDate();
+  const timeStart = addDuration(date);
+  const timeEnd = addDuration(timeStart);
+  return {
+    type,
+    date,
+    timeStart,
+    timeEnd,
+    price: generatePrice(),
+    description: generateDescription(),
+    destination: generateDestination(),
+    offers: getOffers(type),
+    isFavorite: Boolean(getRandomInteger(0, 1)),
+    photos: generatePhotos()
+  };
+};
